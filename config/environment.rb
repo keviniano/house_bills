a5dc7520b5b819e33d4482ed2ca1d8ac8cd14d58
Kevin Chambers <KChambers@RideConnection.org>
2011-07-07 22:15:34 -0700

    change default date format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 HouseBills::Application.initialize!
+
+Date::DATE_FORMATS[:default] = "%m-%d-%Y"
+Time::DATE_FORMATS[:default] = "%m-%d-%Y %H:%M"
+
