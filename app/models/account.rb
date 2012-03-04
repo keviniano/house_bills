84923dcbbc1327f0003528ee1f79cb6677a821ab
Kevin Chambers <KChambers@RideConnection.org>
2012-03-03 16:58:12 -0800

    Fixed formatting of bill shares, added destroy flag on account has_many relationships.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,24 +1,21 @@
 class Account < ActiveRecord::Base
   stampable
-  has_many :bills
+  has_many :bills, :dependent => :destroy
   has_many :shareholder_bills
   has_many :account_bills
-  has_many :account_entries
+  has_many :account_entries, :dependent => :destroy
   has_many :bill_account_entries
   has_many :shareholder_account_entries
   has_many :unbound_account_entries
+  has_many :balance_entries, :dependent => :destroy
   has_many :account_offset_balance_entries
   has_many :bill_offset_balance_entries
   has_many :bill_share_balance_entries
   has_many :shareholders, :dependent => :destroy
   has_many :users, :through => :shareholders
-  has_many :payees
-  has_many :balance_entries
-  has_many :bill_types
+  has_many :payees, :dependent => :destroy
+  has_many :bill_types, :dependent => :destroy
 
   validates_presence_of :name
-
-  accepts_nested_attributes_for :shareholders, :allow_destroy => true, 
-      :reject_if => proc { |attrs| attrs[:name].blank? && attrs[:email].blank? }
 
 end
