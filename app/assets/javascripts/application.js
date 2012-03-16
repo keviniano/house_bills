0b56bc7ec43d83a3f0dd9e7b4748f2c53d74f5ab
Kevin Chambers <KChambers@RideConnection.org>
2012-03-16 01:00:00 -0700

    Adjust styling to match Twitter Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$(document).ready(function(){
+
+  $(".datepicker").datepicker({ dateFormat: 'yy-mm-dd' });
+
+  $("#accordion").accordion({ collapsible: true, active: false });
+
+});
+
