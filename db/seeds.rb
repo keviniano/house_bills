1b761ed77a0ae8bc75f8f6a18559c5de396ae47d
Kevin Chambers <kc@kevinchambers.com>
2015-01-09 00:28:55 -0800

    Fix up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,39 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-# u = User.find_or_initialize_by_name('KC')
-# if u.new_record?
-#   u.email = "kc@kevinchambers.com"
-#   u.encrypted_password = "$2a$10$HU0K/fky2Xo8nOG.jpTFgeIobMgf9NdCNGmd8kGpkMRotIdvRNkJO"
-#   u.save!
-# end
+Role.create! name: 'User'
+Role.create! name: 'Power User'
+Role.create! name: 'Owner'
 
-# BillType.find_or_create_by_name('Utility')
-# BillType.find_or_create_by_name('Internet')
-# BillType.find_or_create_by_name('Food')
-# BillType.find_or_create_by_name('Misc')
-# BillType.find_or_create_by_name('Supplies')
+u = User.new(
+    name:                   'Test User',
+    email:                  'test@tester.com',
+    password:               '123456',
+    password_confirmation:  '123456'
+    )
+u.skip_confirmation!
+u.save!
 
-# Role.find_or_create_by_name 'User'
-# Role.find_or_create_by_name 'Power User'
-# Role.find_or_create_by_name 'Owner'
+a = Account.create!(
+    name:                   'Seed Account',
+    owner_id:               u.id
+    )
 
-# role_id = Role.find_by_name('User').id
-#
-# Account.find_or_create_by_name :name => 'My Account', :owner_id => 1
-# Account.find_or_create_by_name :name => 'The Other Account', :owner_id => 1
-#
-# account_id = Account.find_by_name('My Account').id
-#
-# Shareholder.find_or_create_by_name(:name => 'Kevin', :opened_on => Date.today, :account_id => account_id, :role_id => role_id,
-#     :status => "Account Holder", :status_on => Date.today, :user_id => 1)
-# Shareholder.find_or_create_by_name(:name => 'Steve', :opened_on => Date.today, :account_id => account_id, :role_id => role_id,
-#     :status => "Owner-created", :status_on => Date.today)
-# Shareholder.find_or_create_by_name(:name => 'Virgil', :opened_on => Date.today, :account_id => account_id, :role_id => role_id,
-#     :status => "Owner-created", :status_on => Date.today)
-# Shareholder.find_or_create_by_name(:name => 'Aaron', :opened_on => Date.today, :account_id => account_id, :role_id => role_id,
-#     :status => "Owner-created", :status_on => Date.today)
-# Shareholder.find_or_create_by_name(:name => 'Jim', :opened_on => Date.today, :account_id => account_id, :role_id => role_id,
-#     :status => "Owner-created", :status_on => Date.today)
-# Shareholder.find_or_create_by_name(:name => 'The Pot', :opened_on => Date.today, :account_id => account_id,
-#     :status => "System-created", :status_on => Date.today, :is_pot => true)
+Shareholder.create!(
+    name:                   'Kevin Chambers',
+    email:                  'test@tester.com',
+    account_id:             a.id,
+    user_id:                u.id,
+    role_id:                Role.owner.id,
+    opened_on:              Date.today
+    )
+
+BillType.create!(
+    name:                   'Seed Bill Type',
+    account_id:             a.id
+    )
+
+Payee.create!(
+    name:                   'Seed Payee',
+    account_id:             a.id
+    )
