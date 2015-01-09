d27c5af7213d0b6a8aaf72ffc8c05ffb39d2db9f
Kevin Chambers <kc@kevinchambers.com>
2015-01-09 01:11:58 -0800

    Remove cruft

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-// Javascript to enable link to tab
 $(document).ready(function(){
 
   $("#search-toggle").click(function(event){
