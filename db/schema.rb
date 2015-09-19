333e28d2029cc259c15bea3abf7f4121563f7d4a
Kevin Chambers <kc@kevinchambers.com>
2015-09-19 15:09:09 -0700

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20150102214049) do
   enable_extension "plpgsql"
   enable_extension "fuzzystrmatch"
 
-  create_table "account_entries", force: true do |t|
+  create_table "account_entries", force: :cascade do |t|
     t.string   "type",              limit: 25
     t.integer  "bill_id"
     t.integer  "balance_offset_id"
@@ -36,17 +36,17 @@ ActiveRecord::Schema.define(version: 20150102214049) do
     t.integer  "updater_id"
   end
 
-  create_table "accounts", force: true do |t|
-    t.string   "name"
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name",         limit: 255
     t.integer  "owner_id"
-    t.integer  "lock_version", default: 0
+    t.integer  "lock_version",             default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "creator_id"
     t.integer  "updater_id"
   end
 
-  create_table "balance_entries", force: true do |t|
+  create_table "balance_entries", force: :cascade do |t|
     t.string   "type",             limit: 25
     t.integer  "account_id"
     t.integer  "bill_id"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 20150102214049) do
   add_index "balance_entries", ["account_entry_id"], name: "index_balance_entries_on_account_entry_id", using: :btree
   add_index "balance_entries", ["bill_id"], name: "index_balance_entries_on_bill_id", using: :btree
 
-  create_table "balance_events", force: true do |t|
+  create_table "balance_events", force: :cascade do |t|
     t.date    "date"
     t.integer "account_id"
     t.integer "bill_id"
@@ -77,17 +77,17 @@ ActiveRecord::Schema.define(version: 20150102214049) do
   add_index "balance_events", ["bill_id"], name: "index_balance_events_on_bill_id", using: :btree
   add_index "balance_events", ["date"], name: "index_balance_events_on_date", using: :btree
 
-  create_table "bill_types", force: true do |t|
+  create_table "bill_types", force: :cascade do |t|
     t.integer  "account_id"
-    t.string   "name"
-    t.integer  "lock_version", default: 0
+    t.string   "name",         limit: 255
+    t.integer  "lock_version",             default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "creator_id"
     t.integer  "updater_id"
   end
 
-  create_table "bills", force: true do |t|
+  create_table "bills", force: :cascade do |t|
     t.string   "type",           limit: 25
     t.integer  "bill_type_id",                                                    null: false
     t.integer  "shareholder_id"
@@ -103,10 +103,10 @@ ActiveRecord::Schema.define(version: 20150102214049) do
     t.integer  "updater_id"
   end
 
-  create_table "payees", force: true do |t|
-    t.string   "name"
+  create_table "payees", force: :cascade do |t|
+    t.string   "name",         limit: 255
     t.integer  "account_id"
-    t.integer  "lock_version", default: 0
+    t.integer  "lock_version",             default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "creator_id"
@@ -115,22 +115,22 @@ ActiveRecord::Schema.define(version: 20150102214049) do
 
   add_index "payees", ["name"], name: "index_payees_on_name", unique: true, using: :btree
 
-  create_table "roles", force: true do |t|
-    t.string   "name"
+  create_table "roles", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "shareholders", force: true do |t|
+  create_table "shareholders", force: :cascade do |t|
     t.integer  "account_id"
     t.integer  "user_id"
     t.integer  "role_id"
     t.string   "name",           limit: 50
-    t.string   "email"
-    t.date     "opened_on",                             null: false
+    t.string   "email",          limit: 255
+    t.date     "opened_on",                              null: false
     t.date     "inactivated_on"
     t.date     "closed_on"
-    t.integer  "lock_version",              default: 0
+    t.integer  "lock_version",               default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "creator_id"
@@ -139,26 +139,26 @@ ActiveRecord::Schema.define(version: 20150102214049) do
 
   add_index "shareholders", ["user_id", "account_id"], name: "index_shareholders_on_user_id_and_account_id", unique: true, using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email",                              default: "", null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                   limit: 255
+    t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 128, default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "password_salt"
-    t.string   "confirmation_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "password_salt",          limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.integer  "failed_attempts",                    default: 0
-    t.string   "unlock_token"
+    t.string   "unlock_token",           limit: 255
     t.datetime "locked_at"
-    t.string   "authentication_token"
+    t.string   "authentication_token",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -169,7 +169,7 @@ ActiveRecord::Schema.define(version: 20150102214049) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
-  create_table "versions", force: true do |t|
+  create_table "versions", force: :cascade do |t|
     t.string   "item_type",  null: false
     t.integer  "item_id",    null: false
     t.string   "event",      null: false
