5979b5418719d447910095947a31181fabb25b4a
Kevin Chambers <kc@kevinchambers.com>
2015-09-27 21:17:26 -0700

    Show creation and update timestamps for bills

--- app/models/bill_share_balance_entry.rb
+++ app/models/bill_share_balance_entry.rb
@@ -3,6 +3,8 @@ class BillShareBalanceEntry < BalanceEntry
   belongs_to :account
   belongs_to :balance_event
 
+  after_update :touch_parent_bill
+
   validates_presence_of :shareholder_id
   validates_numericality_of :share, :greater_than_or_equal_to => 0, :message => "must be a number 0 or higher"
 
@@ -13,5 +15,11 @@ class BillShareBalanceEntry < BalanceEntry
     def shareholder(s)
       where(:shareholder_id == s.id).first
     end
+  end
+
+  private
+
+  def touch_parent_bill
+    bill.touch
   end
 end
